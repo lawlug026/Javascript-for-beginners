df674f0c6cc365e090cba1081be06b65b0d54f60
lawlug <ujwalgoyal.2@gmail.com>
2018-01-17 01:49:24 +0530

    output & statements

---
+++ Output & Statements/main.js
@@ -0,0 +1,6 @@
+// Displaying data on webpage using Javascript
+
+document.getElementById("demo").innerHTML="Power Of Javascript";
+alert("Welcome to my page");
+document.write("This is a javascript tutorial");
+console.log("Web page is working fine");
