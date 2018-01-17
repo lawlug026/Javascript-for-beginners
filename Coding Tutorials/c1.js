8241c58ba35cbfa045c728fdfc518017e1f7032d
lawlug <ujwalgoyal.2@gmail.com>
2018-01-17 23:59:59 +0530

    Coding Tutorials Part 1

---
+++ Coding Tutorials/c1.js
@@ -0,0 +1,29 @@
+//To print all the inetgers between 1 to 1000 which do not contain 7
+
+var i, flag;
+for(i=0; i<1000; i++)
+{
+	var b = i;
+	var temp;
+	while(b>0)
+	{
+		temp=b%100;
+		if(temp==22){flag=1;}
+		b=parseInt(b/10);
+	}
+	if(flag==1){
+		flag=0;
+		continue;
+	}
+	document.write(i+" ");
+}
+
+
+
+// var a=245, b;
+// while(a>0){
+// b=a%10;
+// document.write(b+" ");
+// a=parseInt(a/10);
+// // document.write(a);
+// }
